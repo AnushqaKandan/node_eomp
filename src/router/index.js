1c02d2ac0846acac878c4317d9200c767835f043
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-08-19 16:40:49 +0200

    frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductsView from '@/views/ProductsView.vue'
+import ContactView from '@/views/ContactView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: ProductsView
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactView
   },
   {
     path: '/about',
