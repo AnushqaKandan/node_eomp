ca9fc0b0530ffa657766be2a9c09209a6ef5c9f5
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-08-21 12:15:25 +0200

    home

--- src/store/index.js
+++ src/store/index.js
@@ -5,16 +5,37 @@ const apiURL = 'https://node-eomp-u5mv.onrender.com/'
 
 export default createStore({
   state: {
-    products:null
+    products:null,
+    carousel:null,
+    news:null
+
   },
   getters: {
   },
   mutations: {
     setProducts(state,payload){
       state.products = payload;
+    },
+    setCarousel(state,payload){
+      state.carousel = payload;
+    },
+    setNews(state,payload){
+      state.news = payload;
     }
   },
   actions: {
+    async getData({commit}){
+      try {
+        let fetchedInfo = await fetch('https://github.com/C0dingForFun/node-json/data/data.json')
+        let data = await fetchedInfo.json()
+        let {carousel,news} = data
+        commit('setCarousel', carousel)  
+        commit('setNews', news)  
+      } 
+      catch (error) {
+        console.log(error);
+      }
+    },
     async getProducts ({commit}) {
       try {
         let {data} = await axios.get(`${apiURL}products`);
