65fe95b3a6d03b320850b7f9f534e5cecd0469bf
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-08-22 17:48:13 +0200

    frontend

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-sm-12" id="about">
+
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-12">
+        <div class="text row">
+          <div class="col">
+            <h2>Our Story</h2>
+          </div>
+            <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure delectus sit quae ducimus illum possimus nulla alias est. Harum nisi odit quidem nemo iste ducimus inventore veritatis perspiciatis labore ullam.</h5>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<style scoped>
+  .about {
+    display:flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #about{
+    
+    background-image: url('https://c0dingforfun.github.io/node-images/Images/About/about.gif');
+    background-size: cover;
+    width:50vw;
+    height:80vh;
+  }
+  .text{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
