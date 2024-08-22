90e8e139a4fba1cbe06fab96dbca64951ef23cd2
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-08-22 09:43:01 +0200

    products

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -7,17 +7,7 @@
 </template>
 <script>
 export default {
-    methods: {
-        getProducts(){
-            this.$store.dispatch('getProducts');
-        },
-        products() {
-            return this.$store.state.products;
-        }
-    },
-    mounted() {
-        this.getProducts();
-    },
+    
 }
 </script>
 <style>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,9 +1,18 @@
 <template>
     <div class="container mt-5">
-        
-       <template #products>
-        <CardComp/>
-       </template>
+        <CardComp>
+            <template #products>
+                <div class="row" v-for="product in products()" :key="product.prodID">
+                    <div class="card" style="width: 18rem;">
+                        <img :src="product.prodUrl" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">{{product.prodName}}</h5>
+                            <a href="#" class="btn btn-primary">View More</a>
+                        </div>
+                    </div>
+                </div>
+            </template>
+        </CardComp>
     </div>
 </template>
 <script>
@@ -11,7 +20,18 @@ import CardComp from '@/components/CardComp.vue';
 export default {
     components:{
         CardComp
-    }
+    },
+    methods: {
+        getProducts(){
+            this.$store.dispatch('getProducts');
+        },
+        products() {
+            return this.$store.state.products;
+        }
+    },
+    mounted() {
+        this.getProducts();
+    },
 }
 </script>
 <style scoped>
