15f073a34dfa108a9983c0d6eac49364be3ba13e
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-08-23 13:55:48 +0200

    frontend

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="footer">
-        <div class="row" >
-            <div class="col-lg-6 col-md-6 col-sm-12">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 py-2">
                 <div>
-                    <img src="https://c0dingforfun.github.io/node-images/Images/Home/Carousel/Watch.png" alt="">
+                    <img src="https://c0dingforfun.github.io/node-images/Images/Logo/Logo.png" alt="">
                 </div>
             </div>
             <div class="col-lg-2 col-md-2 col-sm-12">
-                <h5>Hello World</h5>
+                <h3>Contact</h3>
+                <div>hi</div>
+                <div>hi</div>
+                <div>hi</div>
             </div>
             <div class="col-lg-3 col-md-2 col-sm-12">
                 <h5>Hello World</h5>
@@ -25,7 +28,7 @@ export default {
         background-image: url('https://c0dingforfun.github.io/node-images/Images/Footer/background.jpg');
         background-size: cover;
         width:100vw;
-        height:20vh;
+        height:25vh;
         
     }
     #footer{
@@ -33,8 +36,13 @@ export default {
         justify-content:center;
         align-content:center;
     }
+    .row{
+        padding-top:3vh;
+    }
     .footer img{
-        width:100%;
+        width:170px;
         height:10vh;
+        background:white;
+        border-radius:50%;
     }
 </style>
